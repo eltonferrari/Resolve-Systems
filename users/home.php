09477cef6d14c6f240594ae68317eb2fed71659e
Elton Ferrari <eltonferrari@gmail.com>
2023-03-06 13:32:42 -0300

    New Branch - Version 0.0.2

--- users/home.php
+++ users/home.php
@@ -9,7 +9,7 @@
             <div class="row">
                 <div class="col-sm-3"></div>
                 <div class="col-sm-6 pt-4 text-center text-primary  ">
-                    <h3>Perfil do Usuário</h3>
+                    <h2>Perfil do Usuário</h2>
                 </div>
                 <div class="col-sm-3"></div>
                 <div>
