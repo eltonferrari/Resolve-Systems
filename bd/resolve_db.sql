c5049f29bdacf61ecb890bcc9cc3ed8aeb6f16d7
Elton Ferrari <eltonferrari@gmail.com>
2023-03-13 10:25:42 -0300

    Version 0.0.3 Criação do LogUser no Banco Criação da Class Loguser

--- bd/resolve_db.sql
+++ bd/resolve_db.sql
@@ -17,7 +17,7 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
 	`iduser` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  	`email` varchar(255) COLLATE utf8_unicode_ci NOT NULL UNIQUE,
+  	`email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   	`password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   	`image` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'vazio.png',
   	`type` int(1) NOT NULL DEFAULT 0,
@@ -28,5 +28,20 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO users(email,password,type) value('eltonferrari@gmail.com','e10adc3949ba59abbe56e057f20f883e',1);
+
+--
+-- Estrutura da tabela `log_users`
+--
+
+DROP TABLE IF EXISTS `log_users`;
+CREATE TABLE IF NOT EXISTS `log_users` (
+	`idloguser` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`iduser` int(10) NOT NULL,
+  	`descricao` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+	`active` int(1) NOT NULL DEFAULT 1,
+  	`created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  	PRIMARY KEY (`idloguser`),
+	FOREIGN KEY(iduser) REFERENCES users(iduser)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 COMMIT;
---
+++ users/class_log.php
@@ -0,0 +1,62 @@
+<?php 
+    require_once ("../connections/DBController.php");
+    class Log {
+        private $db_handle;
+        
+        function __construct() {
+            $this->db_handle = new DBController();
+        }
+        
+        function addLogUser($iduser, $descricao) {
+            $query = "INSERT INTO log_users (iduser, descricao) VALUES (?, ?)";
+            $paramType = "is";
+            $paramValue = array($iduser, $descricao);
+            
+            $insertId = $this->db_handle->insert($query, $paramType, $paramValue);
+            return $insertId;
+        }
+        
+        function deleteLogUser($idLogUser, $active) {
+            $query = "UPDATE users 
+                        SET idloguser = ?,
+                            active = ? 
+                        WHERE idloguser = $idLogUser";
+            $paramType = "ii";
+            $paramValue = array($idLogUser, $active);
+            
+            $this->db_handle->update($query, $paramType, $paramValue);
+        }
+        
+        function getLogUserById($idLogUser) {
+            $query = "SELECT * FROM log_user WHERE idloguser = ?";
+            $paramType = "i";
+            $paramValue = array($idLogUser);
+            $result = $this->db_handle->runQuery($query, $paramType, $paramValue);
+            return $result;
+        }
+        
+        function getAllLogUserForCreate() {
+            $sql = "SELECT * FROM log_users ORDER BY created_at";
+            $result = $this->db_handle->runBaseQuery($sql);
+            return $result;
+        }
+        
+        /*
+        function getUserByEmail($email) {
+            $query = "SELECT * FROM users WHERE email = ?";
+            $paramType = "s";
+            $paramValue = array($email);
+            $result = $this->db_handle->runQuery($query, $paramType, $paramValue);
+            return $result;
+        }
+        
+        function getUserByName($name) {
+            $query = "SELECT * FROM users WHERE name LIKE ?";
+            $paramType = "s";
+            $paramValue = array($name);
+            $result = $this->db_handle->runQuery($query, $paramType, $paramValue);
+            return $result;
+        }
+        */
+    }
+?>
